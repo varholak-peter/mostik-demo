6aee61f24284c528f6cb517c52cb0c014d45f7ce
varholak-peter <peter.varholak@gmail.com>
2020-12-03 16:55:19 +0100

    update

--- src/components/HelloWorld.svelte
+++ src/components/HelloWorld.svelte
@@ -1,11 +1,31 @@
 <script>
-  import { count } from "../store/store.js";
+  let registrationStatus = 'initial'
+  let data = null
 
-  /**
-   * Increases the count variable from the store by 1
-   */
-  function incrementCount() {
-    count.set($count + 1);
+  async function registerCb () {
+    registrationStatus = 'pending'
+
+    try {
+      const res = await fetch('http://localhost:16557/register', {method: 'POST'})
+
+      if (res.ok) {
+        registrationStatus = 'registered'
+      }
+    } catch (err) {
+      registrationStatus = 'failed'
+    }
+  }
+
+  async function getDataCb () {
+    data = 'Loading data...'
+
+    try {
+      const res = await fetch('http://localhost:16557')
+      const resData = await res.json()
+      data = resData
+    } catch (err) {
+      data = 'Error fetching data'
+    }
   }
 </script>
 
@@ -15,8 +35,13 @@
   }
 </style>
 
-<div class="flex items-center justify-center h-screen bg-gray-200">
-  <button class="button" on:click={() => incrementCount()}>
-    Count is {$count}
-  </button>
+<div class="flex flex-col items-center justify-center h-screen bg-gray-200">
+  <div class="flex flex-col items-center justify-around h-56">
+
+  <p>Registration status: {registrationStatus}</p>
+  <p>Data: {JSON.stringify(data, null, 2)}</p>
+  <br/>
+  <button class="button w-40" on:click={registerCb}>Register</button>
+  <button class="button w-40" on:click={getDataCb}>Get Data</button>
+  </div>
 </div>
